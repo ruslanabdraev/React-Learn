f8f5bd910142786a70862ced342f4bd358cfdd8d
ruslanabdraev <ruslan.abdraev@gmail.com>
2019-09-01 21:54:30 +0300

    Implemented styled components

--- 28_self_check/components/PokedexItem.js
+++ 28_self_check/components/PokedexItem.js
@@ -1,16 +1,21 @@
 import React from "react";
 import '../stylesheets/Style.css'
+import styled from 'styled-components'
+
+const StyledPokedexItem = styled.div`
+    background-color: cornsilk;
+    width: 400px;
+    border: solid 1px;
+`
 
 export default function PokedexItem({ item, details, OnClick = f => f }) {
   return details.name === item.name ? (
-    <div className="card">
-      <b>
+    <StyledPokedexItem>
       <p>Name: {details.name}</p>
       <p>Weight: {details.weight}</p>
       <p>Height: {details.height}</p>
       <p><img src={details.front_default}></img></p>
-      </b>
-    </div>
+    </StyledPokedexItem>
   ) : (
     <p onClick={()=>OnClick(item.name)}>{item.name}</p>
   );
