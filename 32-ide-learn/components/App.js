aff7258c95f2e5baccd2825aa3d1d37fa048ad42
Ruslan Abdraev <Ruslan_Abdraev@epam.com>
2019-09-09 17:50:20 +0300

    Implemented changing screen

---
+++ 32-ide-learn/components/App.js
@@ -0,0 +1,52 @@
+import {Component} from 'react'
+import IDEList from './IDEList'
+import idelistData from '../data/ide-list'
+import Screen from './Screen'
+import Hotkeys from 'react-hot-keys';
+
+// const App = () =>
+// <Hotkeys keyName="ctrl+k" onKeyDown={(keyName, e, handle)=>{
+//     alert(`keyName: ${keyName}; e: ${e}; handle: ${handle}`)
+//     }}>
+//     <IDEList data={idelistData}></IDEList>
+//     <br/>
+//     <Screen source="../data/VisualStudio.jpg" ></Screen>
+// </Hotkeys>
+
+class App extends Component{
+    constructor(props){
+        super(props)
+
+        this.changeScreen = this.changeScreen.bind(this)
+
+        this.state = { source: "VisualStudio.jpg" }
+    }
+
+    changeScreen(source){
+        if(source === "VisualStudio.jpg"){
+            this.setState({source: "VisualStudio2.jpg"})
+        }
+
+        if(source === "VisualStudio2.jpg"){
+            this.setState({source: "VisualStudio.jpg"})
+        }
+    }
+
+    render()
+    {
+        return <Hotkeys keyName="ctrl+m" onKeyDown={(keyName, e, handle)=>{
+                this.changeScreen(this.state.source)
+                }}>
+                <IDEList data={idelistData}></IDEList>
+                <br/>
+                <Screen source={`../data/${this.state.source}`} ></Screen>
+        </Hotkeys>
+    }
+
+}
+
+
+
+
+
+export default App 
